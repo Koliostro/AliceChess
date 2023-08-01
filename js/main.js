be57e746c1ffa02336636eafe50b68eb41c18112
Роман <senior.starkoff2018@yandex.ru>
2023-08-01 21:31:50 +0300

    Spreding code on some files

--- js/main.js
+++ js/main.js
@@ -1,68 +1,9 @@
-const leftBoard = document.querySelector('.leftBoard')
-const rightBoard = document.querySelector('.rightBoard')
+import * as field from './field.js'
+import * as pice from './pice_staff.js'
 
-function createDiv(className, idName, draggable = false) {
-    let div = document.createElement('div');
-    div.className = className
-    div.id = idName
-    div.draggable = draggable
-    return div
-}
+field.visualFieldGeneration()
 
-let leftField = generationFieldList();
-let rightField = generationFieldList();
+pice.addPice(0,0,'l','pice pawn', 'b_pawn0')
+pice.addPice(0,0,'r', 'pice rock', 'rock_white0')
 
-function generationFieldList() {
-    let field = []
-    for (let i = 0; i < 8; i++) {
-        field.push(['']);
-        for (let j = 0; j < 7; j++) {
-            field[i].push('');
-        }
-    }
-    return field
-}
-
-// i and j it is a adress of cell in two dimensionla array last symbol it adress of board (l)eft or (r)ight
-for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-        if (i%2 === 0) {
-            if (j%2 === 0) {
-                leftBoard.appendChild(createDiv('cell cellWhite', [i,j,'l']))
-                rightBoard.appendChild(createDiv('cell cellWhite', [i,j,'r']))
-            }
-            else {
-                leftBoard.appendChild(createDiv('cell cellBlack', [i,j,'l']))
-                rightBoard.appendChild(createDiv('cell cellBlack', [i,j,'r']))
-            }
-        }
-        else {
-            if (j%2 !== 0) {
-                leftBoard.appendChild(createDiv('cell cellWhite', [i,j,'l']))
-                rightBoard.appendChild(createDiv('cell cellWhite', [i,j,'r']))
-            }
-            else {
-                leftBoard.appendChild(createDiv('cell cellBlack', [i,j,'l']))
-                rightBoard.appendChild(createDiv('cell cellBlack', [i,j,'r']))
-            }
-        }
-    }
-}
-for (let y= 0; y < 8; y++) {
-    addPice(0,y,'l','pawn black', `pawn_black${y}`)
-}
-    
-
-function addPice(x, y, board, pice_name, id) {
-    document.getElementById(`${x},${y},${board}`).appendChild(createDiv(pice_name, id, true))
-    if (board === 'l') {
-        leftField[x][y] = id
-    }
-    else {
-        rightField[x][y] = id
-    }
-}
-
-console.log(leftField)
-
-console.log(document.querySelector(`#pawn_black0`).id)
+pice.move('b_pawn0', 1,1)

c2e05737d723bfb2c777a20fd91334d1bbba5bd4
Роман <senior.starkoff2018@yandex.ru>
2023-08-01 21:35:08 +0300

    Moved here field generation

---
+++ js/field.js
@@ -0,0 +1,52 @@
+export let leftField = generationFieldList();
+export let rightField = generationFieldList();
+
+export const leftBoard = document.querySelector('.leftBoard')
+export const rightBoard = document.querySelector('.rightBoard')
+
+export function createDiv(className, idName, draggable = false) {
+    let div = document.createElement('div');
+    div.className = className
+    div.id = idName
+    div.draggable = draggable
+    return div
+}
+
+export function generationFieldList() {
+    let field = []
+    for (let i = 0; i < 8; i++) {
+        field.push(['']);
+        for (let j = 0; j < 7; j++) {
+            field[i].push('');
+        }
+    }
+    return field
+}
+
+// i and j it is a adress of cell in two dimensionla array last symbol it adress of board (l)eft or (r)ight
+export function visualFieldGeneration () {
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if (i%2 === 0) {
+                if (j%2 === 0) {
+                    leftBoard.appendChild(createDiv('cell cellWhite', [i,j,'l']))
+                    rightBoard.appendChild(createDiv('cell cellWhite', [i,j,'r']))
+                }
+                else {
+                    leftBoard.appendChild(createDiv('cell cellBlack', [i,j,'l']))
+                    rightBoard.appendChild(createDiv('cell cellBlack', [i,j,'r']))
+                }
+            }
+            else {
+                if (j%2 !== 0) {
+                    leftBoard.appendChild(createDiv('cell cellWhite', [i,j,'l']))
+                    rightBoard.appendChild(createDiv('cell cellWhite', [i,j,'r']))
+                }
+                else {
+                    leftBoard.appendChild(createDiv('cell cellBlack', [i,j,'l']))
+                    rightBoard.appendChild(createDiv('cell cellBlack', [i,j,'r']))
+                }
+            }
+        }
+    }
+}

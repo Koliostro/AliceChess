8699623494c2971806f8960ea3b0f298f82f1902
Роман <senior.starkoff2018@yandex.ru>
2023-08-02 09:46:15 +0300

    Connected all up

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ import * as pice from './pice_staff.js'
 
 field.visualFieldGeneration()
 
-pice.addPice(0,0,'l','pice pawn', 'b_pawn0')
-pice.addPice(0,0,'r', 'pice rock', 'rock_white0')
+pice.addPice(0,0, 'l', 'pice pawn', 'b_p_0')
 
-pice.move('b_pawn0', 1,1)
+pice.move('b_p_0', 1,1)

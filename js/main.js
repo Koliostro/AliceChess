deb2040b8f6398ce793e06826d60e1af52e0b485
Роман <senior.starkoff2018@yandex.ru>
2023-08-04 21:37:43 +0300

    Added scelet of movment fot pices

--- js/main.js
+++ js/main.js
@@ -3,6 +3,22 @@ import * as pice from './pice_staff.js'
 
 field.visualFieldGeneration()
 
-pice.addPice(0,0, 'l', 'pice pawn', 'b_p_0')
+pice.addPice(0,0, 'l', 'pice pawn_black', 'b_p_0')
+pice.addPice(0,1, 'l', 'pice pawn_black', 'b_p_1')
+pice.addPice(0,2, 'l', 'pice pawn_black', 'b_p_2')
 
-pice.move('b_p_0', 1,1)
+const allPices = document.querySelectorAll('.pice')
+
+allPices.forEach( pice => {
+    pice.addEventListener('click', moveingForPices)
+})
+
+function moveingForPices (event) {
+    const y = Number(event.target.parentElement.id.charAt(2))
+    const x = Number(event.target.parentElement.id.charAt(0))
+    const board = event.target.parentElement.id.charAt(4)
+
+    console.log(event.target.id)
+
+    pice.pawnMovment(x,y,board, event.target.id)
+}

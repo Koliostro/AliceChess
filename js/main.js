8fcb40c38de08de148385962333b64b448ccdfd0
Роман <senior.starkoff2018@yandex.ru>
2023-08-05 23:48:41 +0300

    Added half worked movment for all pieces

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ import * as pice from './pice_staff.js'
 
 field.visualFieldGeneration()
 
-pice.addPice(0,0, 'l', 'pice pawn_black', 'b_p_0')
-pice.addPice(0,1, 'l', 'pice pawn_black', 'b_p_1')
-pice.addPice(0,2, 'l', 'pice pawn_black', 'b_p_2')
+pice.addPice(3,6, 'l', 'pice bishop_black', 'b_b_0')
+pice.addPice(1,4, 'l', 'pice rock_black', 'b_r_0')
+pice.addPice(1,7, 'l', 'pice knight_white', 'b_n_0')
 
 const allPices = document.querySelectorAll('.pice')
 
@@ -17,8 +17,28 @@ function moveingForPices (event) {
     const y = Number(event.target.parentElement.id.charAt(2))
     const x = Number(event.target.parentElement.id.charAt(0))
     const board = event.target.parentElement.id.charAt(4)
+    let id = event.target.id
 
-    console.log(event.target.id)
+    pice.clear()
 
-    pice.pawnMovment(x,y,board, event.target.id)
+    switch (id.charAt(2)) {
+        case 'p':
+            pice.pawnMovment(x,y,board, id)
+            break
+        case 'r':
+            pice.rockMovment(x,y,board, id)
+            break
+        case 'b':
+            pice.bishopMovment(x,y,board, id)
+            break
+        case 'q':
+            pice.queenMovment(x, y, board, id)
+            break
+        case 'k':
+            pice.kingMovment(x, y, board, id)
+            break
+        case 'n':
+            pice.knightMovment(x, y, board, id)
+            break
+    }
 }
--- js/pice_staff.js
+++ js/pice_staff.js
@@ -1,31 +1,105 @@
 import * as field from './field.js'
 
 export function pawnMovment(x, y, board, piceId) {
-    x = x + 1
-    for (let i = 0; i < 2; i++) {
-        const cell = document.getElementById(`${x},${y},${board}`)
-        cell.classList.add('lighttedCell')
-        cell.addEventListener('click', (event) => {
-            const y = event.target.id.charAt(2)
-            const x = event.target.id.charAt(0)
-            console.log(event.target)
-            if (event.target.classList.contains('lighttedCell')) {
-                move(piceId, x, y)
+    if (piceId.charAt(0) === 'b') {
+        if (x === 1) {
+            ++x
+            for (let i = 0; i < 2; i++) {
+                lightMovableCells (x,y,board,piceId)
+                x++
             }
-        })
-        x++
+        }
+        else {
+            ++x
+            lightMovableCells (x,y,board,piceId)
+        }
+    }
+
+    else {
+        if (x === 6) {
+            --x
+            for (let i = 0; i < 2; i++) {
+                lightMovableCells (x,y,board,piceId)
+                x--
+            }
+        }
+        else {
+            --x
+            lightMovableCells (x,y,board,piceId)
+        }
     }
 }
 
-function clear() {
+export function rockMovment(x, y, board, piceId) {
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if ((j === x) || (i === y)) {
+                lightMovableCells(j,i,board,piceId)
+            }
+        }
+    }
+}
+
+export function bishopMovment(x, y, board, piceId) {
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if (Math.abs(j - x) === Math.abs(i - y)) {
+                lightMovableCells(j,i,board,piceId)
+            }
+        }
+    }
+}
+
+export function queenMovment(x, y, board, piceId) {
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            bishopMovment(x, y, board, piceId)
+            rockMovment(x, y, board, piceId)
+        }
+    }
+}
+
+export function kingMovment(x, y, board, piceId) {
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if (Math.abs(j - x) <= 1 && Math.abs(i - y) <= 1) {
+                lightMovableCells(j, i, board, piceId)
+            }
+        }
+    }
+}
+
+export function knightMovment (x, y, board, piceId) {
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if ((Math.abs(j - x) === 1 && Math.abs(i - y) === 2) || (Math.abs(j - x) === 2 && Math.abs(i - y) === 1)) {
+                lightMovableCells(j, i, board, piceId)
+            }
+        }
+    }
+}
+
+function lightMovableCells (x,y,board,piceId) {
+    const cell = document.getElementById(`${x},${y},${board}`)
+    if (cell.hasChildNodes() !== true) {
+        cell.classList.add('lighttedCell')
+        cell.addEventListener('click', (event) => movingOfPices(event, piceId))
+    }
+}
+
+function movingOfPices(e, piceId) {
+    const y = e.target.id.charAt(2)
+    const x = e.target.id.charAt(0)
+    if (e.target.classList.contains('lighttedCell')) {
+        move(piceId, x, y)
+    }
+}
+
+export function clear() {
     const Cells = document.querySelectorAll('.cell');
     Cells.forEach(element => {
         element.classList.remove('lighttedCell')
-        element.removeEventListener('click', (event) => {
-            const y = event.target.id.charAt(2)
-            const x = event.target.id.charAt(0)
-            console.log('deleted')
-            move(id, x, y)})
+        element.removeEventListener('click', (event) => movingOfPices)
     });
 }
 
@@ -48,12 +122,12 @@ export function move(idOfMovingPice, xEnd, yEnd) {
 }
 
 /* 
-
+ 
 For pices I create some specific id : {(b)lack/(w)hite}_{(p)awn/(r)ock/k(n)ight/(b)ishop/(k)ing/(q)ueen}_{number}
-
+ 
 for example: b_p_0 => black_pawn_0
              w_h_1 => white_knight_1
-
+ 
 */
 
 export function addPice(x, y, board, pice_class, id) {

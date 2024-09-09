fb1f172756cb4c8b2ce1ec0c7399eb8c2a963b1e
Koliostro <senior.starkoff2018@yandex.ru>
2024-09-09 19:54:43 +0300

    Add compilation into executable file

---
+++ main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    win.loadFile('main.html')
+}
+
+app.whenReady().then( () => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})

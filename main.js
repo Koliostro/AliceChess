54a6c2f4c5b9a518e53bac0a7ccc7a5559f51bb1
Koliostro <senior.starkoff2018@yandex.ru>
2024-09-12 00:02:46 +0300

    Vilusal changes of executable and of game itself

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        title: "Alice's chess"
     })
 
+    win.setMenuBarVisibility(false)
     win.loadFile('main.html')
 }
 
